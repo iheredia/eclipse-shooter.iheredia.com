4d289f07bf451463b00777f846a50c261a18025d
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-02 02:03:21 -0300

    Add small spaceship intro animation

--- client_space_shooter_spaceship.js
+++ client_space_shooter_spaceship.js
@@ -37,21 +37,24 @@
     window.addEventListener('game:space-shooter:controls-enable', function () {
       window.addEventListener('keydown', onKeyDown);
       window.addEventListener('keyup', onKeyUp);
+      ship.controlsEnabled = true;
     })
 
     window.addEventListener('game:space-shooter:controls-disable', function () {
       window.removeEventListener('keydown', onKeyDown);
       window.removeEventListener('keyup', onKeyUp);
+      ship.controlsEnabled = false;
     })
 
     function updateShipState() {
       ship.position.x += ship.speed.x;
       ship.position.y += ship.speed.y;
-      ship.position.x = Math.min(ship.position.x , canvas.width/2);
-      ship.position.x = Math.max(ship.position.x , -canvas.width/2);
-      ship.position.y = Math.min(ship.position.y , canvas.height/2);
-      ship.position.y = Math.max(ship.position.y , -canvas.height/2);
-
+      if (ship.controlsEnabled) {
+        ship.position.x = Math.min(ship.position.x , canvas.width/2);
+        ship.position.x = Math.max(ship.position.x , -canvas.width/2);
+        ship.position.y = Math.min(ship.position.y , canvas.height/2);
+        ship.position.y = Math.max(ship.position.y , -canvas.height/2);
+      }
       setTimeout(updateShipState, 10);
     }
     updateShipState();
@@ -71,7 +74,19 @@
       );
       ctx.fill();
     }
-    var event = new CustomEvent('game:draw-routine:add', { detail: { routine: drawRoutine } });
-    window.dispatchEvent(event);
+    var registerDrawRoutineEvent = new CustomEvent('game:draw-routine:add', { detail: { routine: drawRoutine } });
+    window.dispatchEvent(registerDrawRoutineEvent);
+
+    function revealSpaceShip() {
+      ship.speed.x = 0.2 + 5 * ship.position.x / -canvas.width;
+      if (ship.position.x < 0) {
+        setTimeout(revealSpaceShip, 10);
+      } else {
+        ship.speed.x = 0;
+        var controlsEnableEvent = new CustomEvent('game:space-shooter:controls-enable');
+        window.dispatchEvent(controlsEnableEvent);
+      }
+    }
+    setTimeout(revealSpaceShip, 2e3);
   });
 })();
