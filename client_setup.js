2b0eb8e091a900e19a0904d7eb27d323813cc453
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-02 00:31:14 -0300

    Add simple greetings message

--- client_setup.js
+++ client_setup.js
@@ -11,5 +11,10 @@
   }
   window.addEventListener('resize', resizeCanvas);
   resizeCanvas();
+
   ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.fillStyle = '#ff6347';
+  ctx.beginPath();
+  ctx.arc(0, 0, Math.min(canvas.width/2, canvas.height/2), 0, 2 * Math.PI);
+  ctx.fill();
 })();
