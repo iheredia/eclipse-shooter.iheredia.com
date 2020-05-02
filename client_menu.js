a6f59561d56745d7b61db7dfb2134bd262a5e847
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-02 18:35:50 -0300

    Start game when pressing enter

--- client_menu.js
+++ client_menu.js
@@ -1,11 +1,21 @@
 (function () {
   var startButton = document.querySelector('#start-button');
   var canvas = document.querySelector('#main-canvas');
-  startButton.addEventListener('click', function () {
+
+  function startGame() {
     startButton.disabled = true;
     canvas.classList.add('game-active');
-
     var event = new CustomEvent('game:start');
     window.dispatchEvent(event);
-  });
+  }
+  startButton.addEventListener('click', startGame);
+
+  function startOnEnter(e) {
+    if (e.key === 'Enter') {
+      startGame();
+      window.removeEventListener('keypress', startOnEnter);
+    }
+  }
+  window.addEventListener('keypress', startOnEnter);
+
 })();
