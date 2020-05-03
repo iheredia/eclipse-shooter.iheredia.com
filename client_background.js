8c8b66b72a0d5afc720f5d3c199fde609726d110
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-03 02:47:23 -0300

    Add eclipse

--- client_background.js
+++ client_background.js
@@ -3,14 +3,42 @@
   var ctx = canvas.getContext('2d');
 
   // TODO: mutate form and color of the background
-  // TODO: add some kind of eclipse?
+
+  var circleRadius = Math.min(canvas.width/2, canvas.height/2);
+
+  var moon = {
+    x: circleRadius * 2 * Math.cos(Math.PI/4),
+    y: -circleRadius * 2 * Math.sin(Math.PI/4),
+    active: false,
+  }
+  window.addEventListener('game:start-eclipse', function () {
+    var endYPosition = circleRadius * 2 * Math.sin(Math.PI/4);
+    moon.active = true;
+    function moveMoon() {
+      moon.x -= 0.05;
+      moon.y += 0.05;
+      if (moon.y < endYPosition) {
+        setTimeout(moveMoon, 10);
+      } else {
+        moon.active = false;
+      }
+    }
+    moveMoon();
+  });
 
   function drawRoutine() {
     ctx.globalCompositeOperation = 'source-over';
     ctx.fillStyle = '#ff6347';
     ctx.beginPath();
-    ctx.arc(0, 0, Math.min(canvas.width/2, canvas.height/2), 0, 2 * Math.PI);
+    ctx.arc(0, 0, circleRadius, 0, 2 * Math.PI);
     ctx.fill();
+
+    if (moon.active) {
+      ctx.globalCompositeOperation = 'destination-out';
+      ctx.beginPath();
+      ctx.arc(moon.x, moon.y, circleRadius, 0, 2 * Math.PI);
+      ctx.fill();
+    }
   }
   var event = new CustomEvent('game:draw-routine:add', { detail: { routine: drawRoutine } });
   window.dispatchEvent(event);
