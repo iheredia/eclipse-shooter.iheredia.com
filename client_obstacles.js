4bcad3d2d7c55467e994d5115289421438fbfa00
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-03 04:58:33 -0300

    Support multiple start and end game events in the obstacles script

--- client_obstacles.js
+++ client_obstacles.js
@@ -1,9 +1,12 @@
 (function () {
-  window.addEventListener('game:start', function () {
-    var canvas = document.querySelector('#main-canvas');
-    var ctx = canvas.getContext('2d');
+  var canvas = document.querySelector('#main-canvas');
+  var ctx = canvas.getContext('2d');
 
-    var activeObstacles = [
+  var activeObstacles;
+  var step;
+  function setInitialConditions() {
+    step = 'initial'
+    activeObstacles = [
       {
         position: { x: canvas.width * 5, y: - 100 },
         speed: { x: -5 },
@@ -28,74 +31,95 @@
     ];
     var registerCollisionEvent = new CustomEvent('game:register-collision-objects', { detail: { obstacles: activeObstacles } });
     window.dispatchEvent(registerCollisionEvent);
+  }
 
-    function setupEasyObstacles() {
-      for (var i=0; i<200; i++) {
-        var size = 20 + Math.random() * 20;
-        activeObstacles.push({
-          position: {
-            x: canvas.width + Math.random() * canvas.width * 10,
-            y: (Math.random() - 0.5) * canvas.height/2,
-          },
-          speed: { x: - 5 - 100/size },
-          size: size,
-          rotation: 0,
-          alive: true,
-        })
-      }
-      var registerCollisionEvent = new CustomEvent('game:register-collision-objects', { detail: { obstacles: activeObstacles } });
-      window.dispatchEvent(registerCollisionEvent);
+  function setupEasyObstacles() {
+    for (var i=0; i<200; i++) {
+      var size = 20 + Math.random() * 20;
+      activeObstacles.push({
+        position: {
+          x: canvas.width + Math.random() * canvas.width * 10,
+          y: (Math.random() - 0.5) * canvas.height,
+        },
+        speed: { x: - 5 - 100/size },
+        size: size,
+        rotation: 0,
+        alive: true,
+      })
     }
+    var registerCollisionEvent = new CustomEvent('game:register-collision-objects', { detail: { obstacles: activeObstacles } });
+    window.dispatchEvent(registerCollisionEvent);
+  }
 
-    function setupMediumObstacles() {
-      console.log('Medium obstacles');
-    }
+  function setupMediumObstacles() {
+    console.log('Medium obstacles');
+  }
 
-    function setupDifficultObstacles() {
-      console.log('Difficult obstacles');
-    }
+  function setupDifficultObstacles() {
+    console.log('Difficult obstacles');
+  }
 
-    var step = 'scripted';
-    var nextStepFor = {
-      scripted: 'easy',
-      easy: 'medium',
-      medium: 'difficult'
-    }
-    var setupStep = {
-      'easy': setupEasyObstacles,
-      'medium': setupMediumObstacles,
-      'difficult': setupDifficultObstacles,
-    };
-    function updateState() {
-      var allObstaclesWentBy = true;
-      for (var i=0; i<activeObstacles.length; i++) {
-        var obstacle = activeObstacles[i];
-        obstacle.position.x += obstacle.speed.x;
-        obstacle.rotation += 0.01;
-        allObstaclesWentBy = allObstaclesWentBy && obstacle.position.x < -canvas.width;
-      }
-      if (allObstaclesWentBy) {
-        step = nextStepFor[step];
-        setupStep[step]();
-      }
-      setTimeout(updateState, 10);
-    }
-    updateState();
+  var nextStepFor = {
+    initial: 'easy',
+    easy: 'medium',
+    medium: 'difficult',
+    difficult: null
+  }
+  var setupStep = {
+    'easy': setupEasyObstacles,
+    'medium': setupMediumObstacles,
+    'difficult': setupDifficultObstacles,
+  };
 
-    function drawRoutine() {
-      ctx.fillStyle = '#509EB8';
-      for (var i=0; i<activeObstacles.length; i++) {
-        var obstacle = activeObstacles[i];
-        ctx.translate(obstacle.position.x, obstacle.position.y);
-        ctx.rotate(Math.PI/4 + obstacle.rotation);
-        ctx.fillRect(-obstacle.size/2, -obstacle.size/2, obstacle.size, obstacle.size);
-        ctx.rotate(-Math.PI/4);
-        ctx.fillRect(-obstacle.size/2, -obstacle.size/2, obstacle.size, obstacle.size);
-        ctx.rotate(-obstacle.rotation);
-        ctx.translate(-obstacle.position.x, -obstacle.position.y);
-      }
+  var updateStateTimeout;
+  function updateState() {
+    var allObstaclesWentBy = true;
+    for (var i=0; i<activeObstacles.length; i++) {
+      var obstacle = activeObstacles[i];
+      obstacle.position.x += obstacle.speed.x;
+      obstacle.rotation += 0.01;
+      allObstaclesWentBy = allObstaclesWentBy && obstacle.position.x < -canvas.width;
     }
+    if (allObstaclesWentBy) {
+      step = nextStepFor[step];
+      setupStep[step]();
+    }
+    if (step) {
+      updateStateTimeout = setTimeout(updateState, 10);
+    } else {
+      // TODO: trigger boss
+      var eclipseEvent = new CustomEvent('game:start-eclipse');
+      window.dispatchEvent(eclipseEvent);
+    }
+  }
+
+  function drawRoutine() {
+    ctx.fillStyle = '#509EB8';
+    for (var i=0; i<activeObstacles.length; i++) {
+      var obstacle = activeObstacles[i];
+      ctx.translate(obstacle.position.x, obstacle.position.y);
+      ctx.rotate(Math.PI/4 + obstacle.rotation);
+      ctx.fillRect(-obstacle.size/2, -obstacle.size/2, obstacle.size, obstacle.size);
+      ctx.rotate(-Math.PI/4);
+      ctx.fillRect(-obstacle.size/2, -obstacle.size/2, obstacle.size, obstacle.size);
+      ctx.rotate(-obstacle.rotation);
+      ctx.translate(-obstacle.position.x, -obstacle.position.y);
+    }
+  }
+
+  window.addEventListener('game:start', function () {
     var registerDrawRoutineEvent = new CustomEvent('game:draw-routine:add', { detail: { routine: drawRoutine } });
     window.dispatchEvent(registerDrawRoutineEvent);
+
+    setInitialConditions();
+    clearTimeout(updateStateTimeout);
+    updateState();
+  });
+
+  window.addEventListener('game:end', function () {
+    var registerDrawRoutineEvent = new CustomEvent('game:draw-routine:remove', { detail: { routine: drawRoutine } });
+    window.dispatchEvent(registerDrawRoutineEvent);
+
+    clearTimeout(updateStateTimeout);
   });
 })();
