6b398d904bcd85d5dcb315f2dfcb99ab222af72d
Ignacio Heredia <ignacio.nh@gmail.com>
2020-05-03 04:36:45 -0300

    Support multiple start and end game events in the menu script

--- client_menu.js
+++ client_menu.js
@@ -2,18 +2,28 @@
   var startButton = document.querySelector('#start-button');
   var canvas = document.querySelector('#main-canvas');
 
-  function startGame() {
+  window.addEventListener('game:start', function() {
     window.removeEventListener('keypress', startOnEnter);
     startButton.disabled = true;
     canvas.classList.add('game-active');
+  })
+
+  window.addEventListener('game:end', function() {
+    // TODO: improve. Show a game over menu
+    window.addEventListener('keypress', startOnEnter);
+    startButton.disabled = false;
+    canvas.classList.remove('game-active');
+  })
+
+  function dispatchStartGameEvent() {
     var event = new CustomEvent('game:start');
     window.dispatchEvent(event);
   }
-  startButton.addEventListener('click', startGame);
+  startButton.addEventListener('click', dispatchStartGameEvent);
 
   function startOnEnter(e) {
     if (e.key === 'Enter') {
-      startGame();
+      dispatchStartGameEvent();
     }
   }
   window.addEventListener('keypress', startOnEnter);
